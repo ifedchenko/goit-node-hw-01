6fc767ab220a764470b8732d986d7e3ed962a7a8
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-09-02 15:15:46 +0300

    add invokeAction

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ const {
 // getContactById("AeHIrLTr6JkxGE6SN-0Rw");
 // removeContact("AeHIrLTr6JkxGE6SN-0Rw");
 // addContact("John Doe", "john@example.com", "1234567890");
+
+const argv = require("yargs").argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+    case "get":
+      getContactById(id);
+      break;
+    case "add":
+      addContact(name, email, phone);
+      break;
+    case "remove":
+      removeContact(id);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+invokeAction(argv);
