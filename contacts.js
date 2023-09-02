ee8ae0f947d2286293cd4f660a59c30f733ef4cb
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-09-02 15:15:33 +0300

    fix contact methods

--- contacts.js
+++ contacts.js
@@ -1,35 +1,27 @@
-const fs = require("fs/promises");
+const fs = require("fs").promises;
 const path = require("path");
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
 async function listContacts() {
-  try {
-    const data = await fs.readFile(contactsPath, "utf-8");
-    const contacts = JSON.parse(data);
-    console.table(contacts);
-  } catch (error) {
-    console.error("Error reading contacts:", error);
-  }
+  const data = await fs.readFile(contactsPath, "utf-8");
+  const contacts = JSON.parse(data);
+  console.table(contacts);
+  return contacts;
 }
 
 // listContacts();
 
 async function getContactById(contactId) {
-  try {
-    const data = await fs.readFile(contactsPath, "utf-8");
-    const contacts = JSON.parse(data);
+  const data = await fs.readFile(contactsPath, "utf-8");
+  const contacts = JSON.parse(data);
+  const contact = contacts.find(contact => contact.id === contactId);
 
-    const contact = contacts.find(
-      contact => contact.id === contactId
-    );
-    if (!contact) {
-      console.log(`null`);
-      return null;
-    }
-    console.log(contact);
-  } catch (error) {
-    console.error("Error reading contacts:", error);
+  if (!contact) {
+    console.log(`null`);
+    return null;
   }
+  console.table(contact);
+  return contact;
 }
 
 // getContactById("AeHIrLTr6JkxGE6SN-0Rw");
@@ -47,7 +39,7 @@ async function removeContact(contactId) {
     }
 
     const removedContact = contacts.splice(index, 1)[0];
-    // await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
     console.log(`Contact with id ${contactId} removed.`);
     console.log(removedContact);
     return removedContact;
@@ -67,12 +59,13 @@ const addContact = async (name, email, phone) => {
     contacts.push(newContact);
     await fs.writeFile(contactsPath, JSON.stringify(contacts));
     console.log(`Contact ${name} added successfully!`);
+    console.log(newContact);
   } catch (error) {
     console.error("Error adding contact:", error);
   }
 };
 
-// addContact("John Doe", "john@example.com", "1234567890");
+// addContact("slaiuvakjna22jn", "John Doe", "john@example.com", "1234567890");
 
 module.exports = {
   listContacts,
